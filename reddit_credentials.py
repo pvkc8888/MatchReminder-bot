b24973d6ac2a23f953d985a92253f435fa1803a4
Vinay Pulivarthi <pvkc8888@yahoo.com>
2018-03-20 18:11:55 -0400

    credentials file

---
+++ reddit_credentials.py
@@ -0,0 +1,8 @@
+# just fill up the variables with your values and you should be good. If you
+# don't know your variables, you can refer to PRAW official documentation.
+
+client_id = ''
+client_secret = ''
+password = ''
+user_agent = ''
+username = ''
